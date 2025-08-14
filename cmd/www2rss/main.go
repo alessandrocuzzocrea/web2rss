b7b1dbdd509214487aa5004f88cd25dba85a545d
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2025-08-14 17:30:07 +0900

    Refactor main function to improve logging and error handling during application initialization

--- cmd/www2rss/main.go
+++ cmd/www2rss/main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"log"
 
-	"github.com/alessandrocuzzocrea/www2rss/internal/www2rss"
+	"github.com/alessandrocuzzocrea/www2rss/internal/app"
 )
 
 func main() {
-	log.Printf("Starting www2rss v%s...", www2rss.Version)
-	
-	app := www2rss.New()
-	if err := app.Run(); err != nil {
+	log.Printf("Starting www2rss...")
+
+	app, err := www2rss.New()
+	if err != nil {
+		log.Fatalf("Failed to initialize application: %v", err)
+	}
+
+	if err = app.Run(); err != nil {
 		log.Fatalf("Application failed: %v", err)
 	}
 	
