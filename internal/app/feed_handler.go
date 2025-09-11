03e008f4ddbfb0bbc2bae88dd570a5fce68d3a10
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2025-09-11 18:17:12 +0200

    Add item selectors to feed creation and enhance home template with edit links

--- internal/app/feed_handler.go
+++ internal/app/feed_handler.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/alessandrocuzzocrea/www2rss/internal/db"
@@ -20,6 +21,9 @@ func (a *App) handleCreateFeed(w http.ResponseWriter, r *http.Request) {
 
 	name := r.FormValue("name")
 	url := r.FormValue("url")
+	item_selector := r.FormValue("item_selector")
+	title_selector := r.FormValue("title_selector")
+	link_selector := r.FormValue("link_selector")
 
 	if name == "" || url == "" {
 		http.Error(w, "Name and URL are required", http.StatusBadRequest)
@@ -28,9 +32,13 @@ func (a *App) handleCreateFeed(w http.ResponseWriter, r *http.Request) {
 
 	// Insert the new feed into the database
 	_, err := a.queries.CreateFeed(r.Context(), db.CreateFeedParams{
-		Name: name,
-		Url:  url,
+		Name:          name,
+		Url:           url,
+		ItemSelector:  sql.NullString{String: item_selector, Valid: item_selector != ""},
+		TitleSelector: sql.NullString{String: title_selector, Valid: title_selector != ""},
+		LinkSelector:  sql.NullString{String: link_selector, Valid: link_selector != ""},
 	})
+
 	if err != nil {
 		http.Error(w, "Failed to create feed", http.StatusInternalServerError)
 		return
