a6fa52b163401de3421d4d7613c0d281d9122755
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2025-12-20 00:29:12 +0900

    detect tz

--- internal/app/template_funcs.go
+++ internal/app/template_funcs.go
@@ -13,4 +13,10 @@ var templateFuncs = template.FuncMap{
 		// Format: YYYY-MM-DD HH:MM:SS MST
 		return t.Time.Format("2006-01-02 15:04:05 MST")
 	},
+	"isoDate": func(t sql.NullTime) string {
+		if !t.Valid {
+			return ""
+		}
+		return t.Time.Format("2006-01-02T15:04:05Z07:00")
+	},
 }
